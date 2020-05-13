0285a703c6474f31ae455705fab1e9739863e73a
zhangya6 <46500164+zhangya6@users.noreply.github.com>
2020-05-13 16:41:40 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,9 +4,9 @@ import task
 
 class TestCase(unittest.TestCase):
 
-	def test1(self):
-        	expected = "Hello World"
-        	self.assertEqual(task.my_func(), expected)
+    def test1(self):
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
 
 
 
